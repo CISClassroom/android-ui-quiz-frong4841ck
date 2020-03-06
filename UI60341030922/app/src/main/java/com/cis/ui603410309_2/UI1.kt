429a76ade7aa2e12b89d4ddb22ee38cd42c823a1
frong4841ck <58926344+frong4841ck@users.noreply.github.com>
2020-03-06 13:04:48 +0700

    6034103092

---
+++ UI60341030922/app/src/main/java/com/cis/ui603410309_2/UI1.kt
@@ -0,0 +1,13 @@
+package com.cis.ui603410309_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UI1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ui1)
+    }
+
+}
